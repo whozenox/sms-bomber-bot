01827ec300e06246d8c07f6181bb51db4834d67c
whozenox <sarfarazrain08@gmail.com>
2026-08-27 01:23:34 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+# config.py
+
+# Telegram Bot Token
+BOT_TOKEN = "8824559343:AAEkxfz6jvcQAI9QweC-TOgmvY-xs-rXf18"
+
+# Admin ID (Your Telegram ID)
+ADMIN_ID = 8112149031
+
+# ===== YOUR CREDITS =====
+OWNER = "@lordzenox"
+CHANNEL1 = "@zenoxtool"
+CHANNEL2 = "@ghostpyo"
+BOT_NAME = "ZeNoX BOMBER"
+BOT_VERSION = "v3.2-PREMIUM"
+# ========================
+
+# Credit System
+REFERRAL_BONUS = 5
+BOMB_COST = 2
+FREE_COINS = 5
+MAX_SMS_PER_BOMB = 500

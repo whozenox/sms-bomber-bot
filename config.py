ce830be12788a3f12555d292f21f8b8c12213fc5
whozenox <sarfarazrain08@gmail.com>
2026-08-26 20:48:38 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+# config.py
+
+# Telegram Bot Token
+BOT_TOKEN = "8996694548:AAH9I91qQ7d7A1xpNyYXWjt3YIuw9yjPd7s"
+
+# Admin Telegram ID (Sirf aap hi credit add kar sakte ho)
+ADMIN_ID = 8112149031 # @lordzenox ka Telegram User ID
+
+# Credit System
+REFERRAL_BONUS = 5      # Referral par 5 coins
+BOMB_COST = 2           # Har bomb use karne par 2 coins cut
+FREE_COINS = 5          # New user ko free 5 coins
+
+# Channels (Credit)
+CHANNEL1 = "@zenoxtool"
+CHANNEL2 = "@ghostpyo"
+OWNER = "@lordzenox"
